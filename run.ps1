3759c5624ef078527ebd606625ae4e4bbe5cbfa0
David Acker <davidacker3@gmail.com>
2022-12-05 22:31:10 -0500

    Add timing to PS script

--- run.ps1
+++ run.ps1
@@ -3,6 +3,22 @@ $ScriptName = "run.sh"
 
 $Separator = "=" * 35
 
+function Run-Solution-With-Timing() {
+    param (
+        [ScriptBlock]$SolutionScript
+    )
+
+    $StartTime = $(Get-Date)
+    & $SolutionScript
+    $EndTime = $(Get-Date)
+
+    $ElapsedTime = $EndTime - $StartTime
+    $TotalTime = "{0:HH:mm:ss.fff}" -f [DateTime]($ElapsedTime.Ticks)
+
+    Write-Output ""
+    Write-Output "Elapsed Time: ${TotalTime}"
+}
+
 function Run-Solution-For-Day() {
    param (
     [int]$DayNumber
@@ -12,7 +28,9 @@ function Run-Solution-For-Day() {
 
    if ((Test-Path -Path $SolutionDirectory) -and (Test-Path -Path "${SolutionDirectory}/${ScriptName}")) {
     Set-Location $SolutionDirectory
-    & "./${ScriptName}"
+
+    Run-Solution-With-Timing -SolutionScript { & "./${ScriptName}" }
+
     Set-Location - | Out-Null
 
     Write-Output $Separator
@@ -22,5 +40,5 @@ function Run-Solution-For-Day() {
 Write-Output $Separator
 
 for ($i; $i -le 25; $i++) {
-    Run-Solution-For-Day $i
+    Run-Solution-For-Day -DayNumber $i
 }
